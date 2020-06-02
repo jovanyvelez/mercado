cda862ae1284c616ce81c111842e35bfea4221b7
Jovany Velez Arango <jvelez23@misena.edu.co>
2020-06-02 00:00:02 -0500

    Cuarto commit de mi sitio web

--- script/scripts.js
+++ script/scripts.js
@@ -1,41 +1,38 @@
-
-
-
+const formulario = document.getElementById("form-principal");
+const btnPedido = document.getElementById("btn-pedido");
 const elemento1 = document.getElementById("elemento1");
 const elemento2 = document.getElementById("elemento2");
 const elemento3 = document.getElementById("elemento3");
 const elemento4 = document.getElementById("elemento4");
 const elemento5 = document.getElementById("elemento5");
+const ciudad = document.getElementById("ciudad");
+const pedidos = document.getElementById("pedidos");
+const formulari1 = formulario;
+const documento = document.getElementById("documento");
 
-const ciudad = document.getElementById('ciudad');
+const cancelarConsultaPedido = document.getElementById("btn-cerrar");
 
-ciudad.addEventListener('change',()=>{
-    
-    elemento1.classList.remove("noshow");
-    elemento2.classList.remove("noshow");
-    elemento3.classList.remove("noshow");
-    elemento4.classList.remove("noshow");
-    elemento5.classList.remove("noshow");
-});    
+cancelarConsultaPedido.addEventListener("click", ()=>{
+    pedidos.remove();
+    documento.prepend(formulario);
+    pedidos.classList.add("noshow");
+    documento.after(pedidos);
+});
 
-/*let m = 1;
 
-const div = document.getElementById('div');
-const elemento = document.getElementById('ciudad');
-const mensaje = document.createElement("P");
-mensaje.textContent = "No joda, ya estoy trabajando para avanzar"
-mensaje.classList.add('div1-p2');
-mensaje.setAttribute("id","inserto");
-elemento.addEventListener('change',()=>{
-    
-    if(m==1){
-        alert('aqui voy');
-        m++;
-    }else{
-        div.after(mensaje);
-        const nuevo = document.getElementById('inserto');
-        console.log(nuevo);
-    }
-});*/
+btnPedido.addEventListener("click", () => {
+    formulario.remove();
 
+    pedidos.classList.remove("noshow");
+    //pedidos.classList.add("mover");
+  
+});
+
+ciudad.addEventListener("change", () => {
+  elemento1.classList.remove("noshow");
+  elemento2.classList.remove("noshow");
+  elemento3.classList.remove("noshow");
+  elemento4.classList.remove("noshow");
+  elemento5.classList.remove("noshow");
+});
 
