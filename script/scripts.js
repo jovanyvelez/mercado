73805deb7b25f992056b7737aa5705dd8d903fca
Jovany Velez Arango <jvelez23@misena.edu.co>
2020-06-04 01:35:08 -0500

    moviles

--- script/scripts.js
+++ script/scripts.js
@@ -1,38 +1,34 @@
-const formulario = document.getElementById("form-principal");
-const btnPedido = document.getElementById("btn-pedido");
-const elemento1 = document.getElementById("elemento1");
-const elemento2 = document.getElementById("elemento2");
-const elemento3 = document.getElementById("elemento3");
-const elemento4 = document.getElementById("elemento4");
-const elemento5 = document.getElementById("elemento5");
-const ciudad = document.getElementById("ciudad");
+const formulario = document.getElementById("main-form");
+const btnEstadoPedido = document.getElementById("btn-estadoPedido");
 const pedidos = document.getElementById("pedidos");
-const formulari1 = formulario;
-const documento = document.getElementById("documento");
-
+const ciudad = document.getElementById("ciudad");
+const barrios = document.getElementById("barrios");
+const labelAddress = document.getElementById("labelAddress");
+const address = document.getElementById("address");
+const labelBarrio = document.getElementById("labelBarrio");
+const btnCobertura = document.getElementById("cobertura");
+const textInfo = document.getElementById("text-info");
 const cancelarConsultaPedido = document.getElementById("btn-cerrar");
 
-cancelarConsultaPedido.addEventListener("click", ()=>{
-    pedidos.remove();
-    documento.prepend(formulario);
-    pedidos.classList.add("noshow");
-    documento.after(pedidos);
+cancelarConsultaPedido.addEventListener("click", () => {
+  pedidos.remove();
+  documento.prepend(formulario);
+  pedidos.classList.remove("mover");
+  pedidos.classList.add("noshow");
+  documento.after(pedidos);
 });
 
-
-btnPedido.addEventListener("click", () => {
-    formulario.remove();
-
-    pedidos.classList.remove("noshow");
-    //pedidos.classList.add("mover");
-  
+btnEstadoPedido.addEventListener("click", () => {
+  formulario.remove();
+  pedidos.classList.remove("noshow");
+  pedidos.classList.add("mover");
 });
 
 ciudad.addEventListener("change", () => {
-  elemento1.classList.remove("noshow");
-  elemento2.classList.remove("noshow");
-  elemento3.classList.remove("noshow");
-  elemento4.classList.remove("noshow");
-  elemento5.classList.remove("noshow");
+  labelBarrio.classList.remove("noshow");
+  barrios.classList.remove("noshow");
+  labelAddress.classList.remove("noshow");
+  address.classList.remove("noshow");
+  btnCobertura.classList.remove("noshow");
+  textInfo.classList.remove("noshow");
 });
-
